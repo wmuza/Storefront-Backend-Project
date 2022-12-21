17a9bd02f792157b4de4bb16b1799842a76f7c9a
Wilbert Muza <mrwilbertmuza@gmail.com>
2022-12-21 10:29:43 +0200

    Update c_ordersSpec.ts

--- src/models/tests/c_ordersSpec.ts
+++ src/models/tests/c_ordersSpec.ts
@@ -2,30 +2,30 @@ import { OrderStore } from '../orders'
 
 const store = new OrderStore()
 
-describe('Order Model', () => {
+describe('3. Unit testing the Order Model', () => {
   let orderID = '1';
 
-  it('should have an index method', () => {
+  it('3.1 Should have an index method', () => {
     expect(store.index).toBeDefined()
   })
 
-  it('should have a show method', () => {
+  it('3.2 Should have a show method', () => {
     expect(store.show).toBeDefined()
   })
 
-  it('should have a create method', () => {
+  it('3.3 Should have a create method', () => {
     expect(store.create).toBeDefined()
   })
 
-  it('should have a update method', () => {
+  it('3.4 Should have a update method', () => {
     expect(store.update).toBeDefined()
   })
 
-  it('should have a delete method', () => {
+  it('3.5 Should have a delete method', () => {
     expect(store.delete).toBeDefined()
   })
 
-  it('create method should add a order', async () => {
+  it('3.6 Create method should add a order', async () => {
     const result = await store.create({
       status: 'active',
       user_id: 1
@@ -36,12 +36,12 @@ describe('Order Model', () => {
     expect(result).toBeTruthy()
   })
 
-  it('index method should return a list of orders', async () => {
+  it('3.7 Index method should return a list of orders', async () => {
     const result = await store.index()
     expect(result).not.toBe([])
   })
 
-  it('update method should return the updated order', async () => {
+  it('3.8 Update method should return the updated order', async () => {
     const result = await store.update({
       id: orderID,
       status: 'complete',
@@ -51,13 +51,13 @@ describe('Order Model', () => {
     expect(result).toBeTruthy()
   })
 
-  it('show method should return the correct order', async () => {
+  it('3.9 Show method should return the correct order', async () => {
     const result = await store.show(orderID)
 
     expect(result.status).toEqual('complete')
   })
 
-  it('delete method should remove the order', async () => {
+  it('3.10 Delete method should remove the order', async () => {
     store.delete(orderID)
     const result = await store.index()
 
